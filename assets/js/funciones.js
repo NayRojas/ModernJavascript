0f3a1e3b0a5e460ed4ee702cd025dfb3363ff605
Nayllen Soleika Rojas Martinez <nrojasma@LIM-T29KQPXG73.local>
2023-10-11 20:40:53 -0500

    feat: funciones

--- assets/js/funciones.js
+++ assets/js/funciones.js
@@ -1,3 +1,23 @@
-function(){
-    console.log('Hello world');
+// function greet(){
+//     console.log('Hello world');
+// }
+
+function greet( nombre){
+    console.log(arguments); // aunque no funciona en funciones flecha, nos brinda todos los valores que vienen en la funcion
+    console.log('Hello world' + nombre);
 }
+
+const greetArrow = (nombre) => {
+    console.log('Hello flecha ,' + nombre);
+}
+
+// var greet = 123; dara error
+
+// const greet2 = function(){
+//     console.log('Hello world');
+// }
+
+greet('Nay', 30, 2023);
+greetArrow('Nay')
+// greet2();
+
